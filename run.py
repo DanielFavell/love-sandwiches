97ecb8ec1b556fcdd3c375d5703b7359f4c53f5a
Daniel Favell <danfavell97@gmail.com>
2024-02-19 16:22:43 +0000

    get last 5 days of sales

--- run.py
+++ run.py
@@ -64,6 +64,19 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Get last 5 days of sales data for each sandwich
+    and returns the data as a list of lists.
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     run all program functions
@@ -75,4 +88,5 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("welcome to Love Sandwiches Data Automation")
-main()
+# main()
+sales_columns = get_last_5_entries_sales()
